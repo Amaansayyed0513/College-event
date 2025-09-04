8e77a6ffd882c739f5181c76552eb26deea06ef4
Amaansayyed0513 <sayy74226@gmail.com>
2025-09-04 16:39:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -208,3 +208,22 @@ if (document.getElementById("eventForm")) {
 
   renderAdmin();
 }
+const toggleBtn = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggleBtn.textContent = "☀️ Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+    localStorage.setItem("theme", "light");
+  }
+});
+
+
